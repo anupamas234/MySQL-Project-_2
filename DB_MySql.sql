e17cf4a18397ba1a166030d135503c22dbc6f27f
anupamas234 <152990585+anupamas234@users.noreply.github.com>
2024-10-21 21:11:11 +0530

    Add files via upload

---
+++ DB_MySql.sql
@@ -0,0 +1,54 @@
+CREATE DATABASE library;
+USE library;
+CREATE TABLE Branch (
+    Branch_no INT PRIMARY KEY,
+    Manager_Id INT,
+    Branch_address VARCHAR(255),
+    Contact_no VARCHAR(15)
+);
+CREATE TABLE Employee (
+    Emp_Id INT PRIMARY KEY,
+    Emp_name VARCHAR(100),
+    Position VARCHAR(50),
+    Salary DECIMAL(10, 2),
+    Branch_no INT,
+    FOREIGN KEY (Branch_no) REFERENCES Branch(Branch_no)
+);
+CREATE TABLE Customer (
+    Customer_Id INT PRIMARY KEY,
+    Customer_name VARCHAR(100),
+    Customer_address VARCHAR(255),
+    Reg_date DATE
+);
+CREATE TABLE Books (
+    ISBN VARCHAR(13) PRIMARY KEY,
+    Book_title VARCHAR(255),
+    Category VARCHAR(50),
+    Rental_Price DECIMAL(5, 2),
+    Status ENUM('yes', 'no'),
+    Author VARCHAR(100),
+    Publisher VARCHAR(100)
+);
+CREATE TABLE IssueStatus (
+    Issue_Id INT PRIMARY KEY,
+    Issued_cust INT,
+    Issued_book_name VARCHAR(255),
+    Issue_date DATE,
+    Isbn_book VARCHAR(13),
+    FOREIGN KEY (Issued_cust) REFERENCES Customer(Customer_Id),
+    FOREIGN KEY (Isbn_book) REFERENCES Books(ISBN)
+);
+CREATE TABLE ReturnStatus (
+    Return_Id INT PRIMARY KEY,
+    Return_cust INT,
+    Return_book_name VARCHAR(255),
+    Return_date DATE,
+    Isbn_book2 VARCHAR(13),
+    FOREIGN KEY (Return_cust) REFERENCES Customer(Customer_Id),
+    FOREIGN KEY (Isbn_book2) REFERENCES Books(ISBN)
+);
+
+
+
+
+
